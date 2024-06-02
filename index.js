144b9597964c0425ddf96fda63d038c65e681e84
Vuinguyen08 <nguyenvui7196@gmail.com>
2024-06-02 18:58:21 +0900

    sua lai

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //lesson 1
+
 let result = "";
 for (var a = 0; a < 7; a++) {
   result += "#";
@@ -11,19 +12,22 @@ while (input) {
   if (input != +input) {
     alert("ban nhap vao khong phai la so");
     break;
-  } else if (input < 10) {
+  } else if (+input < 10) {
     alert("so ban nhap vao qua nho");
     break;
   }
+  break;
 }
+
 //lesson 3
 
 let input2 = prompt("yeu cau nhap vao 1 so:");
-while (+input2 < 0) {
-  break;
-}
-if (+input > 0 && +input2 < 2) {
+if (input2 <= 0) {
+  alert("nhap vao so nguyen duong");
+} else if (+input > 0 && +input2 < 2) {
   alert("ban nhap vao khong phai so nguyen to");
+} else if (+input2 === 2) {
+  alert("ban nhap vao la so nguyen to");
 } else if (+input2 > 2) {
   for (let a = 2; a < input2; a++) {
     if (input2 % a === 0) {
@@ -32,16 +36,20 @@ if (+input > 0 && +input2 < 2) {
     break;
   }
 }
+
 //lesson 4
-for (let i = 0; i < 9; i++) {
-  let a = "";
-  for (let k = 9; k > i; k--) {
-    a += " ";
+let treeHeight = prompt("nhap vao chieu cao cay thong:");
+if (treeHeight > 0) {
+  for (i = 1; i < treeHeight; i++) {
+    let tree = "";
+    let space = "";
+    for (j = 1; j <= treeHeight - i; j++) {
+      space += " ";
+    }
+
+    for (k = 1; k <= 2 * i - 1; k++) {
+      tree += "*";
+    }
+    console.log(space, tree, space);
   }
-  let tree = "";
-  let star = 2 * i - 1;
-  for (let j = 0; j < star; j++) {
-    tree += "*";
-  }
-  console.log(a + tree);
 }
